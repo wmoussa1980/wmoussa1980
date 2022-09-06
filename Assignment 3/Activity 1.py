eddcff47e3e57a79740cd8d02be88045aaf384dc
wmoussa1980 <111835462+wmoussa1980@users.noreply.github.com>
2022-09-05 22:22:53 -0500

    Update Activity 1.py

--- Assignment 3/Activity 1.py
+++ Assignment 3/Activity 1.py
@@ -1,10 +1,10 @@
 ﻿# this program will calculate weekly, monthly, and yearly gross pay.
 # References:Programming Fundamentals – A Modular Structured Approach, 2nd Edition
 print("Enter rate per hour")
-rateperhour = float(input("rate per hour"))
+rate = float(input())
 print("Enter hours number")
-hoursnumber = float(input("hours"))
-weekly = hoursnumber * rateperhour
+hours = float(input())
+weekly = hours * rate
 monthly = weekly * 4
 yearly = weekly * 52
 print("Weekly pay " + str(weekly))
