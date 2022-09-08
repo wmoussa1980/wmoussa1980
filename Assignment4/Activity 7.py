51074000975a9201be74a65ea588df30aee3e009
wmoussa1980 <111835462+wmoussa1980@users.noreply.github.com>
2022-09-07 21:38:49 -0500

    Add files via upload

---
+++ Assignment4/Activity 7.py
@@ -0,0 +1,6 @@
+print("Enter the name of your dog")
+name = input()
+print("Enter the age of your dog in human years")
+age = float(input())
+dogage = age * 7
+print(name + " is " + str(dogage) + " years old in dog years.")
