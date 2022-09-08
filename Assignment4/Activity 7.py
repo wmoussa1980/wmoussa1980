caf219a8c9a439876927c3fa206c0c9db35aa56f
wmoussa1980 <111835462+wmoussa1980@users.noreply.github.com>
2022-09-07 22:09:11 -0500

    Add files via upload

--- Assignment4/Activity 7.py
+++ Assignment4/Activity 7.py
@@ -1,3 +1,5 @@
+# This program will prompt the user for the name of their dog and its age in human years.
+# This program will calculate and display the age of their dog in dog years, based on the popular myth that one human year equals seven dog years.
 print("Enter the name of your dog")
 name = input()
 print("Enter the age of your dog in human years")
