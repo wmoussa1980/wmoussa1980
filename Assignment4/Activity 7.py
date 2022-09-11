e91010faf16af639b00c5a36a20907d0282d462f
Dave Braunschweig <dave@itrain.ms>
2022-09-11 08:12:13 -0500

    Assignment corrections

--- Assignment4/Activity 7.py
+++ Assignment4/Activity 7.py
@@ -2,14 +2,12 @@
 
 print("Enter the name of your dog")
 name = input()
+
 print("Enter the age of your dog in human years")
 age = float(input())
 
 # Calculate the age of their dog in dog years.
-
 dog_age = age * 7
 
 # display the name and the age of their dog in dog years
-
-
 print(name, "is", str(dog_age), "years old in dog years.")
