c10230afba27826385a9a534bf66a315a94c9b54
wmoussa1980 <111835462+wmoussa1980@users.noreply.github.com>
2022-09-11 23:46:29 -0500

    Add files via upload

---
+++ Assignment 5/Activity 7.py
@@ -0,0 +1,26 @@
+def calculateDogage(age):
+    dogage = age * 7
+    
+    return dogage
+
+def displayResult(name, dogage):
+    print(name + " is " + str(dogage) + " years old in dog years.")
+
+def getAge():
+    print("Enter the age of your dog in human years")
+    age = float(input())
+    
+    return age
+
+def getName():
+    print("Enter the name of your dog")
+    name = input()
+    
+    return name
+
+# Main
+# This program will prompt the user for the name of their dog and its age in human years.
+name = getName()
+age = getAge()
+dogage = calculateDogage(age)
+displayResult(name, dogage)
