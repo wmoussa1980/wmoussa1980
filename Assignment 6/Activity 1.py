0e63619b2df6adc28c6172be3a16ea8d46c5d710
wmoussa1980 <111835462+wmoussa1980@users.noreply.github.com>
2022-09-16 22:10:23 -0500

    Update Activity 1.py

--- Assignment 6/Activity 1.py
+++ Assignment 6/Activity 1.py
@@ -1,1 +1,44 @@
+# This program will calculate weekly, monthly, and yearly gross pay.
+# References:Programming Fundamentals – A Modular Structured Approach, 2nd Edition
 
+
+def calculateWeekly_pay(hours, rate):
+    weekly_pay = hours*rate
+    
+    return weekly_pay
+    
+def calculateMonthly_pay(weekly_pay):
+    monthly_pay= weekly_pay*4
+    
+    return monthly_pay
+    
+def calculateYearly_pay(weekly_pay):
+    yearly_pay= weekly_pay*52
+    
+    return yearly_pay  
+
+def displayResult(hours, rate):
+    print("Weekly pay " , float(weekly_pay))
+    print("Monthly pay " , float(monthly_pay))
+    print("Yearly pay " , float(yearly_pay))
+    
+def getRate():
+    print("Enter rate per hour")
+    rate = float(input())
+    
+    return rate
+
+def getHours():
+    print("Enter hours number")
+    hours=float(input())
+    
+    return hours
+
+# Main
+
+hours= getHours()
+rate= getRate()
+weekly_pay= calculateWeekly_pay(hours,rate)
+monthly_pay= calculateMonthly_pay(weekly_pay)
+yearly_pay= calculateYearly_pay(weekly_pay)
+displayResult(hours, rate)
