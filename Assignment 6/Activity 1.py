9bf91ecdc7706c23ae6e566fdc3e3c3f18bd8549
wmoussa1980 <111835462+wmoussa1980@users.noreply.github.com>
2022-09-17 09:55:19 -0500

    Update Activity 1.py

--- Assignment 6/Activity 1.py
+++ Assignment 6/Activity 1.py
@@ -6,11 +6,13 @@ def calculateWeekly_pay(hours, rate):
     weekly_pay = hours * rate
     
     return weekly_pay
+  
     
 def calculateMonthly_pay(weekly_pay):
     monthly_pay= weekly_pay * 4
     
     return monthly_pay
+   
     
 def calculateYearly_pay(weekly_pay):
     yearly_pay= weekly_pay * 52
@@ -22,22 +24,24 @@ def getRate():
     print("Enter rate per hour")
     rate = float(input())
     
+    
     return rate
+
 
 def getHours():
     print("Enter hours number")
     hours=float(input())
     
-    return hours
     
+    return hours
+
+
 def displayResult(weekly_pay, monthly_pay, yearly_pay):
     print("Weekly pay " , str(weekly_pay))
     print("Monthly pay " , float(monthly_pay))
     print("Yearly pay " , float(yearly_pay))
        
-
 # Main
-
 def main():
     hours= getHours()
     rate= getRate()
