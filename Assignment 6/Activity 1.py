e479b989ab76ba9ac95f2d173538a105f0319b2e
Dave Braunschweig <dave@itrain.ms>
2022-09-22 08:40:15 -0500

    Assignment 6 corrections

--- Assignment 6/Activity 1.py
+++ Assignment 6/Activity 1.py
@@ -1,36 +1,33 @@
 # This program will calculate weekly, monthly, and yearly gross pay.
-# References:Programming Fundamentals – 2nd Edition
+
+# References:
+#   Programming Fundamentals – 2nd Edition
 
 
 def calculate_weekly(hours, rate):
-    weekly_pay = hours * rate
-    
+    weekly_pay = hours * rate    
     return weekly_pay
   
     
 def calculate_monthly(weekly_pay):
     monthly_pay = weekly_pay * 4
-    
     return monthly_pay
    
     
 def calculate_yearly(weekly_pay):
     yearly_pay = weekly_pay * 52
-    
     return yearly_pay  
 
 
 def get_rate():
     print("Enter rate per hour")
     rate = float(input())
-    
     return rate
 
 
 def get_hours():
     print("Enter hours number")
     hours = float(input())
-    
     return hours
 
 
@@ -38,17 +35,16 @@ def display_result(weekly_pay, monthly_pay, yearly_pay):
     print("Weekly pay ", str(weekly_pay))
     print("Monthly pay ", float(monthly_pay))
     print("Yearly pay ", float(yearly_pay))
-       
-# Main
 
 
-def main():
-    
+def main():    
     hours = get_hours()
     rate = get_rate()
+    
     weekly_pay = calculate_weekly(hours, rate)
     monthly_pay = calculate_monthly(weekly_pay)
     yearly_pay = calculate_yearly(weekly_pay)
+    
     display_result(weekly_pay, monthly_pay, yearly_pay)
 
     
