d1e6a5675fba06da091ab57fb1f36e4d25bb7a52
wmoussa1980 <111835462+wmoussa1980@users.noreply.github.com>
2022-10-09 16:07:33 -0500

    Add files via upload

---
+++ Assignment 8/Activity1.py
@@ -0,0 +1,20 @@
+def forloop(value, number):
+    for n in range(1, number + 1, 1):
+        print(str(value) + "x" + str(n) + "=" + str(value * n))
+
+def getnumber():
+    print("enter expresion number")
+    number = float(input())
+    
+    return number
+
+def getvalue():
+    print("enter value")
+    value = float(input())
+    
+    return value
+
+# Main
+number = getnumber()
+value = getvalue()
+forloop(value, number)
