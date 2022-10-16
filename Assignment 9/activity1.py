8ddc9bb92e6791d197935140864292775cf6b4e5
wmoussa1980 <111835462+wmoussa1980@users.noreply.github.com>
2022-10-16 14:42:08 -0500

    Add files via upload

---
+++ Assignment 9/activity1.py
@@ -0,0 +1,31 @@
+﻿def displayExpression(value, count, product):
+    product = value * count
+    print(str(value) + " * " + str(count) + " = " + str(product))
+
+def getexpressions():
+    print("enter expressions")
+    expressions = int(input())
+    
+    return expressions
+
+def getvalue():
+    print("enter value")
+    value = int(input())
+    
+    return value
+
+def processExpressions(value, expressions):
+    count = 0
+    while count < expressions:
+        product = value * count
+        count = count + 1
+        displayExpression(value, count, product)
+
+# Main
+# # This program will ask the user to enter the value and the number of expressions to be displayed and then uses while loop to generate a list of multiplication expressions for a given value.
+# 
+# # References:
+# #   Programming Fundamentals – 2nd Edition
+value = getvalue()
+expresions = getexpressions()
+processExpressions(value, expresions)
