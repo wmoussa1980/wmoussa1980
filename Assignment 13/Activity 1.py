07ba79c65c438e6b46c2ba28497bcd7e6010dbcc
wmoussa1980 <111835462+wmoussa1980@users.noreply.github.com>
2022-11-14 23:45:17 -0600

    Update Activity 1.py

--- Assignment 13/Activity 1.py
+++ Assignment 13/Activity 1.py
@@ -2,25 +2,30 @@ def get_name():
     name = input("Please Enter your full name:")
     return name
 
+
 def process_input(name):
     name_split = name.split()
     for word in name_split:
         word = word.strip
-        if len(name_split)==1:
+        if len(name_split) == 1:
             display_error()
         else:
             display_name(name_split)
         return name_split
-        
+
+    
 def display_name(name_split):
     print("Your name in the form of (Lastname F) ")
-    print("{}, {}.".format(name_split[1],name_split[0][0]))
+    print("{}, {}.".format(name_split[1], name_split[0][0]))
 
-def display_error():
-     print("please provide complete name or try with space between names")
     
+def display_error():
+    print("please provide complete name or try with space between names")
+ 
+
 def main():
     name = get_name()
     process_input(name)
+
     
 main()  
