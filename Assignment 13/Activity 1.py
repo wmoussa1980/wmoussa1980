521fff8379ac9bee74502c6367bf6903edae9e6a
wmoussa1980 <111835462+wmoussa1980@users.noreply.github.com>
2022-11-15 23:24:04 -0600

    Update Activity 1.py

--- Assignment 13/Activity 1.py
+++ Assignment 13/Activity 1.py
@@ -1,7 +1,8 @@
 # This program will use string functions/methods to parse the line.
 # #  References:
 # #    Programming Fundamentals – 2nd Edition
-#  W3schools Python Tutorial
+# #    Professor Dave Braunschweig
+# #    W3schools Python Tutorial
 
 def get_name():
     name = input("Please Enter your full name:")
