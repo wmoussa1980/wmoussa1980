dd146e61f0ba85d2ba524a053371fa871affb3e3
wmoussa1980 <111835462+wmoussa1980@users.noreply.github.com>
2022-11-15 21:17:44 -0600

    Update Activity 1.py

--- Assignment 13/Activity 1.py
+++ Assignment 13/Activity 1.py
@@ -16,11 +16,11 @@ def split_name(name):
         
 def display_name(name_split):
     print("Your name in the form of (Lastname F) ")
-    print("{}, {}.".format(name_split[1],name_split[0][0]))
+    print("{}, {}.".format(name_split[1], name_split[0][0]))
 
 
 def display_error():
-     print("please provide complete name or try with space between names")
+    print("please provide complete name or try with space between names")
 
     
 def main():
@@ -28,7 +28,7 @@ def main():
     split_name(name)
     name_split = name.split()
     if len(name_split) == 1:
-            display_error()
+        display_error()
     else:
         display_name(name_split)
 
